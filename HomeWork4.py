980062fc2cc826725458c51970966adae437e131
efvalera <efvalera2014@gmail.com>
2024-07-28 21:44:50 +1000

    Add files via upload

---
+++ HomeWork4.py
@@ -0,0 +1,18 @@
+my_string = input ("Какой твой любимый десерт? ")
+print (len(my_string))
+print (my_string .upper())
+print (my_string .lower())
+print (my_string .replace(" ", ""))
+print (my_string [0])
+print (my_string [-1])
+
+
+
+
+#name = input ("Введите Ваше Имя: ")
+#current_year = 2024
+#date_of_birth = (input("В каком году вы родились? "))
+#age = current_year - int (date_of_birth)
+#print ("Здравствуйте,", name)
+#print ("В этом году Вам", age, "лет")
+#print ("Привет, я строка в нижнем регистре" .replace(" ", ""))
